60e3a1ce7883d38e7640a4d21a4bed7ccf545822
Yueming Liu <yl3436@cornell.edu>
2021-10-19 01:17:07 -0400

    Fix bugs in login_page

--- src/login_page.mli
+++ src/login_page.mli
@@ -1,15 +1,15 @@
-type login_infomation
+type login_information
 
 type sign_up_information
 
-exception InvalidEmail of email_name
+exception InvalidEmail of User.email_name
 (** Raised when an invalid email is encountered. *)
 
-exception InvalidUserName of user_name
+exception InvalidUserName of User.user_name
 (** Raised when an invalid username is encountered. *)
 
-val login : login_infomation -> option t 
+val login : login_information -> User.t option
 (** login *)
 
-val sign_up : sign_up_information -> bool
+val sign_up : sign_up_information -> User.t
 (** sign up*)
