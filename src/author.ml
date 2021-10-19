22d9ac4df46342556f0a2351bb068704aa539332
KellyC17 <kellyc2386@gmail.com>
2021-10-19 19:53:48 -0400

    Updated author.ml and author.mli

---
+++ src/author.ml
@@ -0,0 +1,2 @@
+(* hours spent on working for MS1 *)
+let hours_worked = 30
---
+++ src/author.mli
@@ -0,0 +1,11 @@
+(** CS 3110 Fall 2021 Final Project
+
+    @author Kelly Chen kyc34 
+            Yueming Liu yl3436 
+            Daniel Zhou lz489
+            Aaron Ye ajy33 *)
+
+(************************************************************)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours we worked on for MS1 *)
