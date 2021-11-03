d14753335db81766a27df6a0b0fc71569ad1b848
Yueming Liu <yl3436@cornell.edu>
2021-11-03 19:37:39 -0400

    Create Login btn and popup (connected to server)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,28 @@ import LabTabs from './components/Tab';
 import Register from './components/Register';
 import Login from './components/Axios_Login';
 import PostRequest from './components/LoginAxios';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[900]
+    }
+  }
+});
+
 function App() {
+
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Login />
-      <LogB />
-      <MainPanel />
-      {/* <LabTabs /> */}
+      <ThemeProvider theme={theme}>
+        <Login />
+        <MainPanel />
+      </ThemeProvider>
     </div>
   );
 }
--- frontend/src/components/Axios_Login.js
+++ frontend/src/components/Axios_Login.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 export default class Login extends React.Component {
+
   state = {
+    open: false,
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: !this.state.open })
+  }
+
+  handleClose = () => {
+    this.setState({ open: !this.state.open })
   }
 
   handleChange = event => {
@@ -17,29 +35,56 @@ export default class Login extends React.Component {
       password: this.state.password
     };
 
-    axios.post(`http://localhost:5000/CS3110-server/signup`, this.state)
+    axios.post(`http://localhost:5000/CS3110-server/login`, this.state)
       .then(res => {
         console.log(this.state);
         console.log(res);
         console.log(res.data);
         console.log(login);
       })
+
+    this.setState({ open: !this.state.open })
   }
+
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Email:
-            <input type="text" name="email" onChange={this.handleChange} />
-          </label>
-          <label>
-            Password:
-            <input type="text" name="password" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
+        <Button variant="outlined" onClick={this.handleClickOpen}>
+          Login
+        </Button>
+        <Dialog open={this.state.open} onClose={this.handleClose} >
+          <DialogTitle>Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please login with your school email address.
+            </DialogContentText>
+            <TextField
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+              name='email'
+              onChange={this.handleChange}
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              variant="standard"
+              name='password'
+              onChange={this.handleChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose}>Cancel</Button>
+            <Button onClick={this.handleSubmit}>Login</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     )
   }
