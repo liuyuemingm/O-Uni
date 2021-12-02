8a5f333b288c105d4bb9ba8278c85ee3e788af59
Yueming Liu <yl3436@cornell.edu>
2021-12-02 01:58:02 -0500

    Update posts, add types, read from json

---
+++ frontend/src/components/PostData.js
@@ -0,0 +1,57 @@
+export const PostData = [
+  {
+    type: "General Post",
+    title: "Someone threw a turkey off the bridge.",
+    message: "Basically the title. How did this happen??? And I'm mad because they ain't showing basic respects to Turkeys that died for them. ",
+    reaction: "10",
+    author: "frog_11",
+    date: "11/29/2021",
+    responses: [
+      {
+        name: "personA",
+        response: "I saw two pumpkins... ppl are going crazy."
+      },
+      {
+        name: "personB",
+        response: "Post thanksgiving distraught bro."
+      },
+      {
+        name: "personC",
+        response: "Wait which bridge??"
+      },
+      {
+        name: "personC",
+        response: "Are they still there?"
+      },
+    ]
+  },
+  {
+    type: "Course Rating",
+    title: "CS 2800",
+    message: "The prelims... DISATEROUS",
+    reaction: "4.6",
+    author: "df57",
+    date: "10/15/2021",
+    responses: [
+      {
+        name: "personA",
+        response: "I actually find the prove by induction quite useful."
+      },
+      {
+        name: "person",
+        response: "Do you like the instructor? I think Anke is cuuuuute."
+      },
+    ]
+  },
+  {
+    type: "Item Trading",
+    title: "Pet blanket",
+    message: "I have this super comfy fluffy cute blanket I got from Michaels' for $15 and it's still in very good conditions. ",
+    reaction: "8.99",
+    author: "rt909",
+    date: "9/35/2021",
+    responses: [
+
+    ]
+  }
+]
